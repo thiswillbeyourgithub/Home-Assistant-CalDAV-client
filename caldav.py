69057fe0eec2be4e903b974cb512fcc67bd40611
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-10-06 11:48:38 +0200

    add priority and description keys
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- caldav.py
+++ caldav.py
@@ -7,6 +7,8 @@ def caldav_add(
     url: str,
     username: str,
     password: str,
+    priority: int = 3,
+    description: str = "",
     debug: bool = False,
 ):
     """yaml
@@ -16,6 +18,22 @@ fields:
         description: Task summary/title
         example: "Buy groceries"
         required: true
+        selector:
+            text:
+    priority:
+        description: Task priority on a 0 to 10 scale
+        example: 5
+        required: false
+        default: 3
+        selector:
+            number:
+                min: 0
+                max: 10
+    description:
+        description: Task description
+        example: "Important before the wedding"
+        required: false
+        default: ""
         selector:
             text:
     list_uid:
@@ -53,18 +71,23 @@ fields:
     assert password, "caldav password not specified"
     assert username, "caldav username not specified"
     assert list_uid, "caldav list_uid not specified"
+    assert priority, "caldav priority not specified"
 
-    api = task.executor(TasksAPI,
+    api = task.executor(
+        TasksAPI,
         url=url,
         username=username,
         password=password,
         nextcloud_mode=True,
         debug=debug,
     )
-    status = task.executor(api.add_task,
+    status = task.executor(
+        api.add_task,
         task_data=TaskData(
             summary=summary,
-            list_uid=list_uid
+            list_uid=list_uid,
+            priority=priority,
+            description=description,
         ),
         list_uid=list_uid,
     )
