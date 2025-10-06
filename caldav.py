c77cdb4a9ed58dfc5fff6742f12111ba38290008
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-10-06 12:54:15 +0200

    make sure the service returns a response
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- caldav.py
+++ caldav.py
@@ -1,6 +1,6 @@
 from caldav_tasks_api.caldav_tasks_api import TasksAPI, TaskData
 
-@service
+@service(supports_response="only")
 def caldav_add(
     summary: str,
     list_uid: str,
@@ -12,7 +12,7 @@ def caldav_add(
     tags: list[str] = ["HAOS"],
     ssl_verify: bool = True,
     debug: bool = False,
-):
+) -> str:
     """yaml
 description:
     Create a new CalDAV task.
@@ -135,4 +135,4 @@ fields:
         raise
 
     log.info(f"Created task. Status: '{str(status)}'")
-    return status
+    return f"Task creation status: {status}"
